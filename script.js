bf53371f516086868e7abb900d18a6bf90628c95
Olga <spiewakola@gmail.com>
2019-03-15 10:27:00 +0100

    initial commit

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-$(document).ready(function() 
+$(document).ready(function() {
     var cookie = $("#cookie");
 
+    var clicks = 0;
     cookie.on("click", function(){
-        var clicks = 0;
+        
         clicks += 1;
         document.getElementById("clicks").innerHTML = clicks;
         
-    };
+    });
+});
